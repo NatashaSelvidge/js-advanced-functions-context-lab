1ae1507f522958c47745a3d2b64b606805599121
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-03 17:39:19 -0800

    Done.

--- index.js
+++ index.js
@@ -9,14 +9,84 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+ function createEmployeeRecord(employee) {
+   return {
+     firstName: employee[0],
+     familyName: employee[1],
+     title: employee[2],
+     payPerHour: employee[3],
+     timeInEvents: [],
+     timeOutEvents: [],
+   };
+ }
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+ function createEmployeeRecords(employees) {
+   return employees.map(function (e) {
+     return createEmployeeRecord(e);
+   });
+ }
 
-    return payable
-}
+ function createTimeInEvent(dateStamp) {
+   let [date, hour] = dateStamp.split(" ");
+   this.timeInEvents.push({
+     type: "TimeIn",
+     date: date,
+     hour: parseInt(hour, 10),
+   });
+   return this;
+ }
+ // THEY ARE KIND OF SAME BUT JUST TWO DIFFERENT WAYS
+
+const createTimeOutEvent = function (dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    date: date,
+    hour: parseInt(hour, 10),
+  });
+  return this;
+};
+
+ function hoursWorkedOnDate(date) {
+   let timeIn = this.timeInEvents.find(function (record) {
+     return record.date === date;
+   });
+   let timeOut = this.timeOutEvents.find(function (record) {
+     return record.date === date;
+   });
+   return (timeOut.hour - timeIn.hour) / 100;
+ }
+
+ function wagesEarnedOnDate(date) {
+   return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+ }
+
+
+ const findEmployeeByFirstName = (src, employee) => {
+   return src.find((record) => {
+     return record.firstName === employee;
+   });
+ };
+
+ function calculatePayroll(records) {
+   return records.reduce((total, record) => {
+     return total + allWagesFor.call(record);
+   }, 0);
+ } 
+
+
+const allWagesFor = function () {
+  let workDates = this.timeInEvents.map((record) => {
+    return record.date;
+  });
+
+  let payable = workDates.reduce(
+    function (total, date) {
+      return total + wagesEarnedOnDate.call(this, date);
+    }.bind(this),
+    0
+  );
+
+  return payable;
+};
+
